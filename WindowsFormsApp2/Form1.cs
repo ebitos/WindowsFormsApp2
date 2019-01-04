ba4b0dedd2291f9757f053c7e6b78d5360b9efba
ebihara toshiyuki <ugh52910@nifty.com>
2019-01-04 19:46:36 +0900

    MySQLへ接続

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using MySql.Data.MySqlClient;
+
 namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
@@ -19,7 +21,22 @@ namespace WindowsFormsApp2
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Console.Beep();
+            // MySQLへの接続情報
+            string server = "localhost";
+            string user = "root";
+            string pass = "password";
+            string database = "mysql";
+
+            string connectionString = string.Format("Server={0};Database={1};Uid={2};Pwd={3}", server, database, user, pass);
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            DataTable dt = new DataTable();
+
+            MySqlDataAdapter da = new MySqlDataAdapter("select * from test_db.person where address = '東京都'", connection);
+
+            da.Fill(dt);
+
+            dataGridView1.DataSource = dt;
         }
     }
 }
